51e8d359450ea16e20003c7364f36c95b43b5d6d
lindXdeep <linder.igor@gmail.com>
2018-12-18 21:53:57 +0300

    quicksort

--- src/Algorithms/Quick.java
+++ src/Algorithms/Quick.java
@@ -3,11 +3,11 @@ package Algorithms;
 import Sort.ArchetypeSort;
 
 public class Quick extends ArchetypeSort{
-
+    
     public Quick(int[] sourceArray, String nameSort){
         super(sourceArray, nameSort);
     }
-
+       
     @Override
     public void run() {
         
